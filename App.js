391354255b96c5330de40a364a1e511faf8f0234
jasonpellegrini <jpellegrini1616@gmail.com>
2024-01-11 18:25:14 -0500

    Added background

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ImageBackground, TextInput, ActivityIndicator, View } from 'react-native';
+import {useState, useCallBack} from 'react';
+//import axios from 'axios';
 
 export default function App() {
+const [input, setInput] = useState("");
+const [loading, setLoading] = useState(false);
+const [data, setData] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/4knature.jpg')}
+      resizeMode="cover" style={styles.image}> 
+        <View>
+          <TextInput placeholder='Enter city name'
+          onChangeText={text=>setInput(text)}
+          value={input}
+          placeholderTextColor={'#000'}
+          style={styles.textInput}
+          onSubmitEditing={fetchDataHandler}/>
+        </View>
+
+      
+
+      </ImageBackground>
     </View>
   );
 }
@@ -16,5 +34,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  image: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
