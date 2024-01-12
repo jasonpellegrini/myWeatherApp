f72e3682f4540bb3a1701460621afcab8c12bae6
jasonpellegrini <jpellegrini1616@gmail.com>
2024-01-12 16:59:36 -0500

    implemented api

--- App.js
+++ App.js
@@ -1,33 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, ImageBackground, TextInput, ActivityIndicator, View } from 'react-native';
-import {useState, useCallBack} from 'react';
-//import axios from 'axios';
+import React, {useState, useCallBack} from 'react';
+import axios from 'axios';
 
 export default function App() {
 const [input, setInput] = useState("");
 const [loading, setLoading] = useState(false);
 const [data, setData] = useState([]);
 
-/*const fetchDataHandler = React.useCallback(() => {
+const api = {
+  key: '545bc18417d4844bcce4f5e114e4549a',
+  baseUrl: 'http://api.openweathermap.org/data/2.5/',
+};
 
-}, []);*/
+const fetchDataHandler = async () => {
+  try {
+    setLoading(true);
+
+    const response = await axios.get(`${api.baseUrl}weather?q=${input}&appid=${api.key}`);
+    setData(response.data);
+
+    console.log(data);
+
+    setLoading(false);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    setLoading(false);
+  }
+};
 
   return (
     <View style={styles.container}>
       <ImageBackground source={require('./assets/4knature.jpg')}
       resizeMode="cover" style={styles.image}> 
         <View>
-          <TextInput placeholder='Enter city name'
+          <TextInput placeholder='Enter city name...'
           onChangeText={text=>setInput(text)}
           value={input}
           placeholderTextColor={'#000'}
           style={styles.textInput}
-          //onSubmitEditing={fetchDataHandler}
+          onSubmitEditing={fetchDataHandler}
           />
+
         </View>
-
-      
-
+          {loading && 
+          <View>
+            <ActivityIndicator size={"large"} color="#000"></ActivityIndicator>
+          </View>}
       </ImageBackground>
     </View>
   );
